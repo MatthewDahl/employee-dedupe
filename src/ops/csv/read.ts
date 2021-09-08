d02fbbcd013525fa900911b91571c836ab984c1e
Matthew Dahl <64293526+MatthewDahl@users.noreply.github.com>
2021-09-08 14:33:18 -0700

    convert to fast-csv parseFile

--- src/ops/csv/read.ts
+++ src/ops/csv/read.ts
@@ -1,24 +1,25 @@
-import { createReadStream } from 'fs';
+import { resolve } from 'path';
 import { EmployeeRow } from '@src/interfaces';
-import { parse } from '@fast-csv/parse';
+import { parseFile } from '@fast-csv/parse';
 
 /**
  * Read a given CSV file into an object
  */
 export async function read(file: string): Promise<EmployeeRow[]> {
+  const fullPath = resolve(__dirname, file);
+
   const data: EmployeeRow[] = [];
-  const parser = parse<EmployeeRow, EmployeeRow>({
+  const parser = parseFile<EmployeeRow, EmployeeRow>(fullPath, {
     headers: true,
     ignoreEmpty: true,
     discardUnmappedColumns: true,
   });
 
-  await new Promise((resolve, reject) => {
-    createReadStream(file)
-      .pipe(parser)
+  await new Promise<void>((resolve, reject) => {
+    parser
       .on('error', (err) => reject(err))
       .on('data', (row) => data.push(row))
-      .on('end', () => resolve(data));
+      .on('end', () => resolve());
   });
 
   return data;
